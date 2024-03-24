791a62247ef7b4cff166ba985a0b211cd31a5b3a
Dasha Khmelkova <khmelkovadaria@gmail.com>
2024-03-24 16:38:30 +0300

    add: create simple test

---
+++ src/test/java/org/example/SimpleTest.java
@@ -0,0 +1,32 @@
+package org.example;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.concurrent.TimeUnit;
+
+public class SimpleTest {
+
+    public static WebDriver driver;
+    @BeforeClass
+    public static void setup() {
+        System.setProperty("webdriver.chrome.driver", ConfProperties.getProperty("chromedriver"));
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.get(ConfProperties.getProperty("page"));
+    }
+
+    @Test
+    public void loginTest() {
+        driver.findElement(By.id("name")).sendKeys("Daria Kozina");
+    }
+
+    @AfterClass
+    public static void tearDown() {
+        driver.quit(); }
+}
