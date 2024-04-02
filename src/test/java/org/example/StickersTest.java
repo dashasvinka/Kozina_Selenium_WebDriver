63130b716a92a292006cb23c37d9b5f513df05dc
Dasha Khmelkova <khmelkovadaria@gmail.com>
2024-04-02 18:46:55 +0300

    add: автотест на проверку количества стикеров

---
+++ src/test/java/org/example/StickersTest.java
@@ -0,0 +1,49 @@
+package org.example;
+
+import org.junit.AfterClass;
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+public class StickersTest{
+
+    public static WebDriver driver;
+    @BeforeClass
+    public static void setup() {
+        System.setProperty("webdriver.chrome.driver", ConfProperties.getProperty("chromedriver"));
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.get(ConfProperties.getProperty("lite"));
+    }
+
+    @Test
+    public void checkStickers() {
+        //Логинимся
+        driver.findElement(By.name("email")).sendKeys("dkhmelkova@mail.ru");
+        driver.findElement(By.name("password")).sendKeys("dkhmelkova");
+        driver.findElement(By.name("login")).click();
+        //Ищем все карточки товара
+        List<WebElement> products = driver.findElements(By.cssSelector("[class^=product]"));
+        int sizeProducts = products.size();
+        for (int b = 0; b < sizeProducts; b++) {
+            //Ищем стикеры в карточке
+            List<WebElement> stickers = products.get(b).findElements(By.cssSelector("[class^=sticker]"));
+            //Проверяем, что в карточке только один стикер
+            int stickersQuantityExpected = 1;
+            int stickersQuantityActual = stickers.size();
+            Assert.assertEquals(stickersQuantityExpected, stickersQuantityActual);
+        }
+    }
+
+    @AfterClass
+    public static void tearDown() {
+        driver.quit(); }
+}
